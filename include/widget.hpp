cc95108b138d6c6213dc07d96d9aba62f6507764
kyyneogs <oks990304@gmail.com>
2024-03-11 19:41:53 +0900

    feat: QLayout

--- include/widget.hpp
+++ include/widget.hpp
@@ -1,11 +1,22 @@
 #ifndef WIDGET_H
 #define WIDGET_H
 
+#include <string>
 #include <QWidget>
+#include <QPushButton>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QGridLayout>
 
 class Widget : public QWidget
 {
     Q_OBJECT
+
+private:
+    QPushButton *btn[4];
+    QHBoxLayout *hbox;
+    QGridLayout *gbox;
+    QVector<QPushButton *> vect;
 
 public:
     Widget(QWidget *parent = nullptr);
--- src/widget.cpp
+++ src/widget.cpp
@@ -2,6 +2,23 @@
 
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
-{}
+{
+    setWindowTitle("QLayout");
+        
+    for(int i=0; i<4; i++){
+        std::string s = "It is Num ";
+        btn[i] = new QPushButton(this);
+        s += std::to_string(i+1);
+        btn[i]->setText(s.c_str());
+        btn[i]->setFixedSize(200, 200);
+    }
+
+    gbox = new QGridLayout(this);
+    for(int i=0; i<2; i++){
+        for(int j=0; j<2;j++){
+            gbox->addWidget(btn[2*i+j], i, j);
+        }
+    }
+}
 
 Widget::~Widget() {}
